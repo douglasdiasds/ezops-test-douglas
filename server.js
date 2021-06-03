8e682ebe415a8f0f43ba739cceb32ca8b32ba7d2
unknown <diasdasilvadouglas12@gmail.com>
2021-06-03 11:19:14 -0300

    feature: Adding mongoose(mongo db) configurations

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
-var express = require(‘express’);
+var express = require('express');
 var app = express();
 var server = app.listen(3000, () => {
- console.log(‘server is running on port’, server.address().port);
+ console.log('server is running on port', server.address().port);
 });
 app.use(express.static(__dirname));
+
+var mongoose = require('mongoose');
+var dbUrl = 'mongodb://username:pass@ds257981.mlab.com:57981/simple-chat'
+mongoose.connect(dbUrl , (err) => { 
+   console.log('mongodb connected',err);
+})
+var Message = mongoose.model('Message',{ name : String, message : String})
+
